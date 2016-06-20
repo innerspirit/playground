f05b9a3fc40a8b8785c2e359dc8a9118723abb77
Christian Maioli <mmchristian@gmail.com>
2016-06-20 11:44:23 -0300

    Imported Spring Security Oauth2

--- src/main/java/org/acme/sample/Application.java
+++ src/main/java/org/acme/sample/Application.java
@@ -2,8 +2,10 @@ package org.acme.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class Application {
 
 	public static void main(String[] args) {
