5d0f5a650b09aa2d7003efbe78c346493a65f8c0
Christian Maioli <mmchristian@gmail.com>
2016-06-23 22:40:48 -0300

    Inserted fake user to test the endpoints

--- src/test/java/org/acme/sample/controller/SampleControllerTest.java
+++ src/test/java/org/acme/sample/controller/SampleControllerTest.java
@@ -1,44 +1,70 @@
 package org.acme.sample.controller;
 
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
-
 import org.acme.sample.Application;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
 @WebIntegrationTest(randomPort = true)
 public class SampleControllerTest {
 
-	private TestRestTemplate restTemplate = new TestRestTemplate();
+	private RestTemplate restTemplate;
 
 	@Value("${local.server.port}")
     private int port;
-	
-	@Test
-	public void hello() {
-		ResponseEntity<String> entity = this.restTemplate.getForEntity(
-				getUrl("/samples/hello?name=world"), String.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());
-		assertEquals("Hello world", entity.getBody());
-	}
+
+    private DefaultOAuth2ClientContext clientContext;
+
+    private ResourceOwnerPasswordResourceDetails resourceDetails;
+
+    @Before
+    public void setUp() {
+        resourceDetails = new ResourceOwnerPasswordResourceDetails();
+        resourceDetails.setUsername("chris");
+        resourceDetails.setPassword("prueba");
+        resourceDetails.setAccessTokenUri(format("http://localhost:%d/oauth/token", port));
+        resourceDetails.setClientId("acme");
+        resourceDetails.setClientSecret("acmesecret");
+        resourceDetails.setGrantType("password");
+        resourceDetails.setScope(asList("read", "write"));
+
+        clientContext = new DefaultOAuth2ClientContext();
+    }
 
 	@Test
-	public void helloWithoutParameter() {
+	public void hello() {
+        restTemplate = new OAuth2RestTemplate(resourceDetails, clientContext);
+		ResponseEntity<String> entity = this.restTemplate.getForEntity(
+				getUrl("/samples/hello"), String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertEquals("Hello chris", entity.getBody());
+	}
+
+	@Test(expected = HttpClientErrorException.class)
+	public void helloWithoutUser() {
+        restTemplate = new RestTemplate();
 		ResponseEntity<String> entity = this.restTemplate
 				.getForEntity(getUrl("/samples/hello"), String.class);
-		assertEquals(HttpStatus.BAD_REQUEST, entity.getStatusCode());
+		assertEquals(HttpStatus.UNAUTHORIZED, entity.getStatusCode());
 	}
 
 	private String getUrl(String path) {
-		return "http://localhost:" + port + path;  
+		return "http://localhost:" + port + "/api" + path;
 	}
 }
